7b55a383b9dd903dc48cfcdd90fed008366a5f71
Alex Pelavin <ajpele@gmail.com>
2016-11-12 03:29:11 -0800

    Created reddit.py

---
+++ reddit.py
@@ -0,0 +1,18 @@
+import requests
+import requests.auth
+
+auth = open('auth','r')
+clientID = auth.readline()[:-1]
+secret   = auth.readline()[:-1]
+username = 'covie21'
+
+client_auth = requests.auth.HTTPBasicAuth(clientID,secret)
+post_data   = {'grant_type':'client_credentials'}
+headers     = {'User-Agent': 'client'}
+response    = requests.post('https://www.reddit.com/api/v1/access_token', auth=client_auth, data=post_data, headers=headers).json()
+
+token      = response['access_token']
+token_type = response['token_type']
+
+headers  = {'Authorization': token_type+' '+token, 'User-Agent': 'client'}
+response = requests.get('https://oauth.reddit.com/user/'+username+'/comments', headers=headers).json()
